ac0b04b22f379e139f6ccd5dcad6902d5a947c26
savamurra <franki-55@mail.ru>
2026-05-05 15:40:33 +0600

    feat: add firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import {environment} from '../environments/enviroments';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
-};
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+  ]
+}
---
+++ src/environments/enviroments.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAGgLxQl9COoyPc62xaZ4hdjcfvimSGkSU",
+    authDomain: "pool-service-b9ab1.firebaseapp.com",
+    projectId: "pool-service-b9ab1",
+    storageBucket: "pool-service-b9ab1.firebasestorage.app",
+    messagingSenderId: "230366707212",
+    appId: "1:230366707212:web:c3a4d5d82713aff82bf829"
+  }
+}
