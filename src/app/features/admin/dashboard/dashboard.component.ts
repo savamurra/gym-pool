fbb6bdea79728396ed3e780ebdaf63078f02e353
savamurra <franki-55@mail.ru>
2026-05-06 17:01:59 +0600

    fix: downgrade firebase to 10.14.1 to fix collectionData query error

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BookingService} from '../../../core/services/booking.service';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {Booking, BookingStatus} from '../../../core/models/pool.models';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  private bookingService = inject(BookingService)
 
+  bookings = toSignal(this.bookingService.getBookings(), {initialValue: [] as Booking[]})
+
+  totalBookings = computed(() => this.bookings().length)
+
+  newBookings = computed(() =>
+    this.bookings().filter(booking => booking.status === 'new').length)
+
+  confirmedBookings = computed(() =>
+    this.bookings().filter(booking => booking.status === 'confirmed').length)
+
+  cancelledBookings = computed(() =>
+    this.bookings().filter(booking => booking.status === 'cancelled').length)
+
+  recentBookings = computed(() =>
+  this.bookings().slice(0, 5))
+
+  getStatusLabel(status: BookingStatus): string {
+    const labels: Record<BookingStatus, string> = {
+      'new': 'Новая',
+      'processing': 'В обработке',
+      'confirmed': 'Подтверждена',
+      'cancelled': 'Отменена'
+    }
+    return labels[status]
+  }
 }
